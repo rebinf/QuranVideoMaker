ae563f81dd80c1d98bcadc4802933781fcf8f67e
Rebin <rebinf@outlook.com>
2023-07-29 04:37:04 +0100

    apply project settings

--- src/QuranVideoMaker/Dialogs/ViewModels/ProjectSettingsViewModel.cs
+++ src/QuranVideoMaker/Dialogs/ViewModels/ProjectSettingsViewModel.cs
@@ -6,61 +6,67 @@ using System.Windows.Forms;
 
 namespace QuranVideoMaker.Dialogs.ViewModels
 {
-    /// <summary>
-    /// Project Settings ViewModel
-    /// </summary>
-    /// <seealso cref="INotifyPropertyChanged" />
-    [Description("Project Settings ViewModel")]
-    [DisplayName("ProjectSettingsViewModel")]
-    [DebuggerDisplay("ProjectSettingsViewModel")]
-    public partial class ProjectSettingsViewModel : DialogViewModelBase
-    {
-        private ResolutionProfile _selectedProfile;
+	/// <summary>
+	/// Project Settings ViewModel
+	/// </summary>
+	/// <seealso cref="INotifyPropertyChanged" />
+	[Description("Project Settings ViewModel")]
+	[DisplayName("ProjectSettingsViewModel")]
+	[DebuggerDisplay("ProjectSettingsViewModel")]
+	public partial class ProjectSettingsViewModel : DialogViewModelBase
+	{
+		private ResolutionProfile _selectedProfile;
 
-        /// <summary>
-        /// Gets the project.
-        /// </summary>
-        /// <value>
-        /// The project.
-        /// </value>
-        public Project Project { get; }
+		/// <summary>
+		/// Gets the project.
+		/// </summary>
+		/// <value>
+		/// The project.
+		/// </value>
+		public Project Project { get; }
 
-        public ResolutionProfile SelectedProfile
-        {
-            get { return _selectedProfile; }
-            set
-            {
-                if (_selectedProfile != value)
-                {
-                    _selectedProfile = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
+		public ResolutionProfile SelectedProfile
+		{
+			get { return _selectedProfile; }
+			set
+			{
+				if (_selectedProfile != value)
+				{
+					_selectedProfile = value;
+					OnPropertyChanged();
+				}
+			}
+		}
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="SelectTranslationViewModel"/> class.
-        /// </summary>
-        public ProjectSettingsViewModel(Project project)
-        {
-            Project = project;
-            SelectedProfile = ResolutionProfile.Presets.FirstOrDefault(x => x.Width == Project.Width && x.Height == Project.Height);
-        }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SelectTranslationViewModel"/> class.
+		/// </summary>
+		public ProjectSettingsViewModel(Project project)
+		{
+			Project = project;
+			SelectedProfile = ResolutionProfile.Presets.FirstOrDefault(x => x.Width == Project.Width && x.Height == Project.Height);
+		}
 
-        [RelayCommand]
-        private void OnBrowseProjectDirectory()
-        {
-            var dlg = new FolderBrowserDialog();
+		[RelayCommand]
+		private void OnBrowseProjectDirectory()
+		{
+			var dlg = new FolderBrowserDialog();
 
-            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                Project.ExportDirectory = dlg.SelectedPath;
-            }
-        }
+			if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				Project.ExportDirectory = dlg.SelectedPath;
+			}
+		}
 
-        public override void OnOK()
-        {
-            CloseWindow?.Invoke(true);
-        }
-    }
+		public override void OnOK()
+		{
+			Project.Width = SelectedProfile.Width;
+			Project.Height = SelectedProfile.Height;
+			Project.FPS = SelectedProfile.FPS;
+			Project.PreviewWidth = SelectedProfile.PreviewWidth;
+			Project.PreviewHeight = SelectedProfile.PreviewHeight;
+
+			CloseWindow?.Invoke(true);
+		}
+	}
 }
