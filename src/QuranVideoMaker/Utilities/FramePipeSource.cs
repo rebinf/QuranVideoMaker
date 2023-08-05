bc41e52359f36832103125313323b3ba0307993a
Rebin <rebinf@outlook.com>
2023-08-05 16:15:55 +0100

    use IEnumerable

--- src/QuranVideoMaker/Utilities/FramePipeSource.cs
+++ src/QuranVideoMaker/Utilities/FramePipeSource.cs
@@ -3,36 +3,32 @@ using System.IO;
 
 namespace QuranVideoMaker.Utilities
 {
-    public class FramePipeSource : IPipeSource
-    {
-        public double FPS { get; set; }
+	public class FramePipeSource : IPipeSource
+	{
+		public double FPS { get; set; }
 
-        private readonly IEnumerator<byte[]> _frames;
+		private readonly IEnumerable<byte[]> _frames;
 
-        public FramePipeSource(IEnumerator<byte[]> frames, double fps = 25)
-        {
-            FPS = fps;
-            _frames = frames;
-        }
+		public FramePipeSource(IEnumerable<byte[]> frames, double fps = 25)
+		{
+			FPS = fps;
+			_frames = frames;
+		}
 
-        public FramePipeSource(IEnumerable<byte[]> frames, double fps = 25) : this(frames.GetEnumerator(), fps) { }
+		public string GetStreamArguments()
+		{
+			return $"-f image2pipe -framerate {FPS} ";
+		}
 
-        public string GetStreamArguments()
-        {
-            return $"-f image2pipe -framerate {FPS} ";
-        }
+		public async Task WriteAsync(Stream outputStream, CancellationToken cancellationToken)
+		{
+			var array = _frames.ToArray();
 
-        public async Task WriteAsync(Stream outputStream, CancellationToken cancellationToken)
-        {
-            if (_frames.Current != null)
-            {
-                await outputStream.WriteAsync(_frames.Current, 0, _frames.Current.Length, cancellationToken);
-            }
-
-            while (_frames.MoveNext())
-            {
-                await outputStream.WriteAsync(_frames!.Current, 0, _frames.Current!.Length, cancellationToken);
-            }
-        }
-    }
+			for (int i = 0; i < _frames.Count(); i++)
+			{
+				var current = array[i];
+				await outputStream.WriteAsync(current, 0, current.Length, cancellationToken);
+			}
+		}
+	}
 }
