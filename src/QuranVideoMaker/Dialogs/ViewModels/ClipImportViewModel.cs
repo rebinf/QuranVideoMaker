2c827503e05e04d132b2689e8c2818ca14c969d8
Rebin <rebinf@outlook.com>
2024-11-22 21:53:09 +0000

    import elapsed time

--- src/QuranVideoMaker/Dialogs/ViewModels/ClipImportViewModel.cs
+++ src/QuranVideoMaker/Dialogs/ViewModels/ClipImportViewModel.cs
@@ -1,10 +1,19 @@
 ﻿using QuranVideoMaker.Data;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Timers;
 using System.Windows;
 
 namespace QuranVideoMaker.Dialogs.ViewModels
 {
     public class ClipImportViewModel : DialogViewModelBase
     {
+        private Stopwatch _importTime;
+        private System.Timers.Timer _importTimer;
+        private string _elapsed;
+
         public ProjectClip Clip { get; }
 
         private double _progress;
@@ -22,10 +31,25 @@ namespace QuranVideoMaker.Dialogs.ViewModels
             }
         }
 
+        public string Elapsed
+        {
+            get { return _elapsed; }
+            set
+            {
+                if (_elapsed != value)
+                {
+                    _elapsed = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
         public ClipImportViewModel(ProjectClip clip)
         {
             Clip = clip;
             Clip.CacheProgress += Clip_CacheProgress;
+            _importTimer = new System.Timers.Timer(1000);
+            _importTimer.Elapsed += ImportTimer_Elapsed;
             Import();
         }
 
@@ -33,6 +57,8 @@ namespace QuranVideoMaker.Dialogs.ViewModels
         {
             Task.Factory.StartNew(() =>
             {
+                _importTime = Stopwatch.StartNew();
+                _importTimer.Start();
                 Clip.CacheFrames();
             }).ContinueWith(x => Application.Current.Dispatcher.Invoke(() => this.CloseWindow.Invoke(true)));
         }
@@ -40,6 +66,11 @@ namespace QuranVideoMaker.Dialogs.ViewModels
         private void Clip_CacheProgress(object sender, double e)
         {
             Progress = e;
+        }
+
+        private void ImportTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Elapsed = _importTime.Elapsed.ToString(@"hh\:mm\:ss");
         }
 
         public override void OnClosed()
